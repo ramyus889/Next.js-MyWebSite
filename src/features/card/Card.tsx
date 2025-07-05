0ae7a3aeb483dbb3bfc5e2ca612946c08ff342e0
ramyus889 <ramyus889.com>
2025-07-05 13:44:41 +0300

    feat: обновление заголовка сайта и улучшение структуры компонента Card

--- src/features/card/Card.tsx
+++ src/features/card/Card.tsx
@@ -22,7 +22,7 @@ export const Card = ({ title, link, src, gitLink, itemContent }: CardProps) => {
     <div className="w-full">
       <div className=" border-2 rounded-2xl p-4 ">
         <div className="">
-          <div className="flex justify-between items-center">
+          <div className="flex flex-col sm:flex-row sm:justify-between justify-center sm:items-start items-center gap-5">
             <Link href={link} target="_blank" className="inline-block">
               <img
                 src={src}
@@ -30,33 +30,33 @@ export const Card = ({ title, link, src, gitLink, itemContent }: CardProps) => {
                 className="w-30 h-30 object-cover border-2 p-1 rounded-full"
               />
             </Link>
-            <div className="flex flex-col gap-3 items-center">
-              <Link href={gitLink} target="_blank">
-                <BsGithub size={38} />
-              </Link>
-
-              <img
-                src="/frontend.jpg"
-                alt=""
-                className="w-10 h-10 border-2 rounded-full border-gray-300"
-              />
+            <div className="flex flex-wrap sm:justify-end justify-center gap-2">
+              {itemContent.map((item) => (
+                <Link
+                  href={item.link}
+                  key={item.id}
+                  target="_blank"
+                  className="border border-gray-300 rounded-full px-2 py-0.4 gap-1 text-[15px] flex items-center"
+                >
+                  <div className="">{item.icon}</div>
+                  <div className="">{item.language}</div>
+                </Link>
+              ))}
             </div>
           </div>
           <div className="flex justify-between items-end">
             <div className="pt-5 flex items-center justify-between w-full">
               <div className="text-2xl">{title}</div>
-              <div className="flex  gap-2">
-                {itemContent.map((item) => (
-                  <Link
-                    href={item.link}
-                    key={item.id}
-                    target="_blank"
-                    className="border border-gray-300 rounded-full px-2 py-0.4 gap-1 text-[15px] flex items-center"
-                  >
-                    <div className="">{item.icon}</div>
-                    <div className="">{item.language}</div>
-                  </Link>
-                ))}
+              <div className="flex gap-3 items-center">
+                <Link href={gitLink} target="_blank">
+                  <BsGithub size={38} />
+                </Link>
+
+                <img
+                  src="/frontend.jpg"
+                  alt=""
+                  className="w-10 h-10 border-2 rounded-full border-gray-300"
+                />
               </div>
             </div>
           </div>
