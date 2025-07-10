874f9159a8a15ca014b3f2ead6602ab72261768e
ramyus889 <ramyus889.com>
2025-07-10 11:57:50 +0300

    refactor: удалить экспорт компонента SplitText из общего интерфейса

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,3 @@
-export { SplitText } from "./ReactBitsUI/SplitText/SplitText";
 export { FuzzyText } from "./ReactBitsUI/FuzzyText/FuzzyText";
 export { RotatingText } from "./ReactBitsUI/RotatingText/RotatingText";
 export { IconText } from "./ReactBitsUI/IconText/IconText";
