b8ad41f3a675b790fd1bd6ca12caaabf3be4139a
ramyus889 <ramyus889.com>
2025-07-10 14:24:23 +0300

    refactor: упростить условный рендеринг в компоненте NotFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,45 +15,24 @@ export default function NotFound() {
   }, []);
   return (
     <div className="flex flex-col items-center mt-[200px] gap-5 overflow-hidden">
-      {isResized ? (
-        <>
-          <FuzzyText
-            baseIntensity={0.2}
-            hoverIntensity={0.5}
-            enableHover={true}
-            fontSize={130}
-          >
-            404
-          </FuzzyText>
-          <FuzzyText
-            baseIntensity={0.2}
-            hoverIntensity={0.5}
-            enableHover={true}
-            fontSize={45}
-          >
-            Not Found
-          </FuzzyText>
-        </>
-      ) : (
-        <>
-          <FuzzyText
-            baseIntensity={0.2}
-            hoverIntensity={0.5}
-            enableHover={true}
-            fontSize={200}
-          >
-            404
-          </FuzzyText>
-          <FuzzyText
-            baseIntensity={0.2}
-            hoverIntensity={0.5}
-            enableHover={true}
-            fontSize={75}
-          >
-            Not Found
-          </FuzzyText>
-        </>
-      )}
+      <>
+        <FuzzyText
+          baseIntensity={0.2}
+          hoverIntensity={0.5}
+          enableHover={true}
+          fontSize={isResized ? 130 : 200}
+        >
+          404
+        </FuzzyText>
+        <FuzzyText
+          baseIntensity={0.2}
+          hoverIntensity={0.5}
+          enableHover={true}
+          fontSize={isResized ? 45 : 75}
+        >
+          Not Found
+        </FuzzyText>
+      </>
     </div>
   );
 }
