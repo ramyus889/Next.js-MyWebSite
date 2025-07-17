8d1e8acf3f5665f373fed50eaea46acaf007db97
ramyus889 <ramyus889.com>
2025-07-17 12:20:45 +0300

    refactor: Header add Link and style

--- src/widget/Header/header.tsx
+++ src/widget/Header/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { BsGithub } from "react-icons/bs";
 import { IoMdContact } from "react-icons/io";
 import { DiTrello } from "react-icons/di";
 import { AiOutlineHome } from "react-icons/ai";
@@ -8,11 +9,32 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { routes } from "@/shared/routes";
 
 const items = [
-  { id: "1", icon: <AiOutlineHome size={30} />, href: "/" },
-  { id: "2", icon: <DiTrello size={30} />, href: "/pages/About" },
-  { id: "3", icon: <IoMdContact size={30} />, href: "/pages/Contact" },
+  { id: "1", icon: <AiOutlineHome size={30} />, href: routes.home, target: "" },
+  { id: "2", icon: <DiTrello size={30} />, href: routes.level, target: "" },
+  {
+    id: "3",
+    icon: <IoMdContact size={30} />,
+    href: routes.contact,
+    target: "",
+  },
+];
+
+const itemsLink = [
+  {
+    id: "1",
+    icon: <img src="/hhLogo.png" className="w-[30px] h-[30px]" alt="" />,
+    href: "https://hh.ru/?hhtmFrom=resume",
+    target: "_blank",
+  },
+  {
+    id: "2",
+    icon: <BsGithub size={30} className="text-white" />,
+    href: "https://github.com/ramyus889",
+    target: "_blank",
+  },
 ];
 
 export const Header = () => {
@@ -21,11 +43,11 @@ export const Header = () => {
   return (
     <div
       className={cn(
-        "fixed top-0 -right-60 pt-5 max-w-80 w-full ps-2 z-1000 transition-all duration-300 ease-in-out",
+        "fixed top-0 -right-62 pt-5 max-w-80 w-full ps-2 z-1000 transition-all duration-300 ease-in-out",
         isClick && "-right-1"
       )}
     >
-      <div className="border-2 border-r-0 rounded-l-full h-13 flex justify-between bg-black ps-[6px] pe-6 items-center gap-3">
+      <div className="border-2 border-r-0 rounded-l-full h-13 flex  bg-black ps-[6px]  items-center gap-3">
         <button onClick={() => setIsClick(!isClick)} className="">
           <img
             src="/frontend.jpg"
@@ -33,18 +55,35 @@ export const Header = () => {
             className="w-10 h-10 rounded-full border-2 border-gray-300"
           />
         </button>
-
-        {items.map((item) => (
-          <Link
-            key={item.id}
-            href={item.href}
-            className={cn(
-              router === item.href ? "text-white font-bold" : "text-gray-400"
-            )}
-          >
-            {item.icon}
-          </Link>
-        ))}
+        <div className="flex items-center gap-4 ml-[4px] mr-[1px]">
+          {items.map((item) => (
+            <Link
+              key={item.id}
+              href={item.href}
+              target={item.target}
+              className={cn(
+                router === item.href ? "text-white font-bold" : "text-gray-400"
+              )}
+            >
+              {item.icon}
+            </Link>
+          ))}
+        </div>
+        <div className="w-[2px] h-12 bg-gray-300"></div>
+        <div className="flex items-center gap-4 ml-[5px]">
+          {itemsLink.map((item) => (
+            <Link
+              key={item.id}
+              href={item.href}
+              target={item.target}
+              className={cn(
+                router === item.href ? "text-white font-bold" : "text-gray-400"
+              )}
+            >
+              {item.icon}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
